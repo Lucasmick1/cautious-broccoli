bebc4cd3d06d084625851ca954214a918ac0da04
Lucasmick1 <lucas_29_costa@hotmail.com>
2021-10-06 21:01:03 -0300

    refatoração

--- forca.py
+++ forca.py
@@ -1,31 +1,65 @@
 import random
 
-
-def create_spaces(palavra):
-    spaces = []
-    for i in palavra:
-        spaces.append("_")
-    return spaces
-
-
 def get_file(fileName):
     file = open(fileName, "r")
     return file
 
+def get_secret_word():
+    index = random.randrange(0, 5)
+    file = get_file('palavras.txt')
+    palavras = file_to_array(file)
+    return palavras[index]
+
+def create_spaces(palavra):
+    return ["_" for i in palavra]
 
 def file_to_array(file):
     array = [word.strip() for word in file]
     file.close()
     return array
 
+def input_attempt():
+    return input("Qual a letra? ").strip().lower()
 
-def get_secret_word():
-    index = random.randrange(0, 5)
-    file = get_file('palavras.txt')
-    palavras = file_to_array(file)
+def print_char(spaces):
+    print(" O")
+    print("\|/")
+    print(" |")
+    print("/ \\")
+    print(" ".join(spaces))
+    print("")
 
-    return palavras[index]
+def print_you_won(secret_word):
+    print("""
+     Você acertou !!! 
+     A palavra era: {}
+     """.format(secret_word))
 
+def print_you_lose(secret_word):
+    print("""
+        Poxa, você perdeu :(
+        A palavra era: {}
+        """.format(secret_word))
+
+def check_if_won(spaces, secret_word):
+    secret_word_join = str("".join(spaces))
+    if secret_word_join == secret_word:
+        return True
+    return False
+
+def fill_spaces(secret_word, emptySpaces, attempt):
+    i = 0
+    spaces = emptySpaces
+    for char in secret_word:
+        if char == attempt:
+            spaces[i] = attempt
+        i += 1
+    return spaces
+
+def has_attempts(attempts):
+    if attempts > 0:
+        return True
+    return False
 
 def jogar():
 
@@ -34,31 +68,24 @@ def jogar():
 
     dead = False
     win = False
-
+    attempts = len(secret_word)
+   
     while not dead and not win:
-        print(" O")
-        print("\|/")
-        print(" |")
-        print("/ \\")
-        print(" ".join(spaces))
-        print("")
-        chute = input("Qual a letra? ").strip().lower()
-        i = 0
-        for char in secret_word:
-            if char == chute:
-                spaces[i] = chute
+        attempts -= 1
+        
+        print_char(spaces)
 
-            i = i + 1
+        attempt = input_attempt()
+        spaces = fill_spaces(secret_word, spaces, attempt)
 
-        stage = str("".join(spaces))
-        if stage == secret_word:
+        if check_if_won(spaces, secret_word):
             win = True
-            print("""
-                  
-                  ACERTOU !!! A PALAVRA ERA
-                  {}
-                  """.format(secret_word))
-
+            print_you_won(secret_word)
+            
+        if not has_attempts(attempts):
+            dead = True
+            print_you_lose(secret_word)
+    
     return
 
 
